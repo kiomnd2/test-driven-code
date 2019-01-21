6548d505a5bfd691b38e9ab6eb27598508e1c8c1
DESKTOP-J3NFBAH\kiomn <kiomnd2@naver.com>
2019-01-21 23:18:27 +0900

    Maven 및 travis 설정

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,9 +1,8 @@
-import java.io.IOException;
-
 public class test {
+    static void main(String[] args) throws Exception{
 
-    static void main(String[] args) throws IOException {
         System.out.println("test");
         System.out.println("test");
+
     }
 }
